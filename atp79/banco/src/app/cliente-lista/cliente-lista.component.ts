34f8bc773c16ff49c637840f94d327c664766351
Joanne PSilva <joannepassos@gmail.com>
2021-12-07 14:51:14 -0300

    ATP79 - model, interfaces

---
+++ atp79/banco/src/app/cliente-lista/cliente-lista.component.ts
@@ -0,0 +1,16 @@
+import { ClienteService } from './../services/cliente.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente-lista',
+  templateUrl: './cliente-lista.component.html',
+  styleUrls: ['./cliente-lista.component.css']
+})
+export class ClienteListaComponent implements OnInit {
+
+  constructor(private cliente:ClienteService) { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ atp79/banco/src/app/tipocliente/tipocliente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tipocliente',
+  templateUrl: './tipocliente.component.html',
+  styleUrls: ['./tipocliente.component.css']
+})
+export class TipoclienteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
